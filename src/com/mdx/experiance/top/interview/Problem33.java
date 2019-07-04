05154d6ad3d5922d6fb3660a329d64bdb1430c86
liuweikai <liuweikai@meituan.com>
2019-07-04 10:49:05 +0800

    33

--- src/com/mdx/experiance/top/interview/Problem33.java
+++ src/com/mdx/experiance/top/interview/Problem33.java
@@ -30,7 +30,7 @@ import com.mdx.experiance.problem.ProblemConstructBuilder;
  * @see
  * @since 2019/7/1
  */
-@Info(status = StatusEnum.WRONG_ANSWER)
+@Info(status = StatusEnum.ACCEPTTED)
 @Mark
 public class Problem33 {
 
@@ -38,15 +38,10 @@ public class Problem33 {
         if (nums.length == 0) {
             return -1;
         }
-        if (nums.length == 1) {
-            if (nums[0] == target) {
-                return 0;
-            }
-            return -1;
-        }
 
         // 翻转位置 最小值所在位置
-        Integer rotateIndex = findRotateIndex(nums);
+        Integer rotateIndex = findRotateIndexV2(nums);
+
         if (nums[rotateIndex] == target) {
             return rotateIndex;
         } else if (nums[rotateIndex] > target) {
@@ -76,48 +71,63 @@ public class Problem33 {
                 return mid;
             }
             if (nums[mid] > target) {
-                st = mid + 1;
-            } else {
                 et = mid - 1;
+            } else {
+                st = mid + 1;
             }
         }
         return -1;
-
     }
 
     /**
-     * 找出翻转位置
+     * 找出翻转位置 也就是最小值所在的索引
+     * 时间复杂度 O(n)
      *
      * @param nums
      * @return
      */
     private Integer findRotateIndex(int[] nums) {
-        return findRotateIndex(0, nums.length, nums);
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i - 1] && nums[i] < nums[i + 1]) {
+                return i;
+            }
+        }
+        return nums[0] < nums[nums.length - 1] ? 0 : nums.length - 1;
     }
 
     /**
-     * 找出翻转位置 最小值所在索引
+     * 找出翻转位置 也就是最小值所在的索引
+     * 时间复杂度 O(logN)
      *
-     * @param left
-     * @param right
      * @param nums
      * @return
      */
-    private Integer findRotateIndex(int left, int right, int[] nums) {
-        int mid = (left + right) >> 1;
-        if (mid != 0 && mid != nums.length - 1 &&
-                nums[mid] < nums[mid - 1] && nums[mid] < nums[mid + 1]) {
-            return mid;
+    private Integer findRotateIndexV2(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        int mid = 0;
+        while (left < right) {
+            mid = (left + right) >> 1;
+            if (nums[mid] > nums[left]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
         }
-        if (nums[mid] > nums[left]) {
-            return findRotateIndex(mid, right, nums);
+        if (nums[mid + 1] < nums[mid]) {
+            return mid + 1;
         }
-        return findRotateIndex(left, mid, nums);
+        // 说明没有过翻转
+        return 0;
+
     }
 
     public static void main(String[] args) {
         Problem33 problem33 = new ProblemConstructBuilder<Problem33>(Problem33.class.getName()).build();
-
-        System.out.println(problem33.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+//        System.out.println(problem33.searchBinary(0, 3, new int[]{1, 3, 5}, 1));
+//        System.out.println(problem33.findRotateIndexV2(new int[]{1, 3, 5}));
+        System.out.println(problem33.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+//        System.out.println(problem33.findRotateIndexV2(new int[]{4, 5, 6, 7, 0, 1, 2}));
+//        System.out.println(problem33.search(new int[]{3, 1}, 1));
+//        System.out.println(problem33.search(new int[]{1}, 0));
     }
 }
